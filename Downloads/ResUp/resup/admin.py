836e09d9d7abdaef5f9fe9a9832d418cd26f0651
Pranav Joshi <pranav.joshi@iitgn.ac.in>
2025-02-06 10:53:01 +0530

    Delete Downloads/ResUp directory

--- Downloads/ResUp/resup/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Company,Job,Student,Application
-# Register your models here.
-
-admin.site.register(Company)
-admin.site.register(Job)
-admin.site.register(Student)
-admin.site.register(Application)
